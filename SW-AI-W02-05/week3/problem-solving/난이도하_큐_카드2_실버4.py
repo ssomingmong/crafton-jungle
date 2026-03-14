19b044ef9c57d07065fae54c9b3fbbda1cac87da
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-14 20:23:17 +0900

    solve: boj_2164

--- SW-AI-W02-05/week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ SW-AI-W02-05/week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,12 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+from collections import deque
+
+N = int(input())
+cards = [i+1 for i in range(N)]
+queue = deque(cards)
+
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+print(queue[0])
