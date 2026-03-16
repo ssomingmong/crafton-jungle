d5d4a67c1d17e560f622f2bf067a7bbbe5d6b14f
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-16 10:24:13 +0900

    solve:boj-9933

--- SW-AI-W02-05/week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ SW-AI-W02-05/week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,16 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dt = {}
+
+
+for i in range(N):
+    pwd = input().strip()
+    dt[pwd] = pwd[::-1]
+    mid = len(pwd) // 2
+
+    if pwd[::-1] in dt :
+        print(f"{len(pwd)} {pwd[mid]}")
