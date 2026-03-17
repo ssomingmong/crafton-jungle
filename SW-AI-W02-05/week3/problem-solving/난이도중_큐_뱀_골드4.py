8974db7b2a63e1d6ab874fc08d3c84666235e8de
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-17 23:36:27 +0900

    solve: boj-3190

--- SW-AI-W02-05/week3/problem-solving/난이도중_큐_뱀_골드4.py
+++ SW-AI-W02-05/week3/problem-solving/난이도중_큐_뱀_골드4.py
@@ -1,2 +1,87 @@
-# 큐 - 뱀 (백준 골드4)
+#뱀 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/3190
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())  
+k = int(input())  
+
+
+board = [[0] * n for _ in range(n)]
+
+
+for _ in range(k):
+    r, c = map(int, input().split())
+    board[r - 1][c - 1] = 1
+
+l = int(input())  # 방향 전환 횟수
+
+
+turns = {}
+for _ in range(l):
+    x, c = input().split()
+    turns[int(x)] = c
+
+
+
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
+
+
+snake = deque()
+snake.append((0, 0))  
+
+
+snake_set = set()
+snake_set.add((0, 0))
+
+direction = 0  
+time = 0       
+
+
+head_r, head_c = 0, 0
+
+while True:
+    time += 1  
+
+
+    nr = head_r + dr[direction]
+    nc = head_c + dc[direction]
+
+    
+    if not (0 <= nr < n and 0 <= nc < n):
+        print(time)
+        break
+
+    
+    if (nr, nc) in snake_set:
+        print(time)
+        break
+
+    
+    snake.appendleft((nr, nc))
+    snake_set.add((nr, nc))
+
+    
+    if board[nr][nc] == 0:
+    
+        tail = snake.pop()
+        snake_set.remove(tail)
+    else:
+    
+        board[nr][nc] = 0  
+
+    
+    head_r, head_c = nr, nc
+
+    
+    if time in turns:
+        if turns[time] == 'L':
+    
+            direction = (direction - 1) % 4
+        else:  
+    
+            direction = (direction + 1) % 4
