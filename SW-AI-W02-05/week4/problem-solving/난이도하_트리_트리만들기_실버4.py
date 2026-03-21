8253bb977bdc41e4e8bf37fc7be9e1bb96ecc89f
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-21 10:24:41 +0900

    slove: boj-14244-트리 만들기

--- SW-AI-W02-05/week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ SW-AI-W02-05/week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,14 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+num = [i for i in range(n)]
+
+for i in range(m):
+    print(0, i + 1)
+
+for j in range(m, n - 1):
+    print(j, j + 1)
