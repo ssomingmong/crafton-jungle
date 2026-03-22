f40db94a5ad5a664db92e29218e7571a1a85cd7d
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-22 19:12:25 +0900

    solve:boj-2606-바이러스

--- SW-AI-W02-05/week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ SW-AI-W02-05/week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,27 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+import sys
+input = sys.stdin.readline
+
+computer = int(input())
+n = int(input())
+graph = {i:[] for i in range(1, computer + 1)}
+visited = [False] * (computer + 1)
+
+for _ in range(n):
+    u ,v = map(int, input().split())
+    
+    graph[u].append(v)
+    graph[v].append(u)
+
+def dfs(node):
+    visited[node] = True
+    count = 0
+
+    for j in graph[node]:
+        if not visited[j]:
+            count += 1
+            count += dfs(j)
+    return count
+
+print(dfs(1))
