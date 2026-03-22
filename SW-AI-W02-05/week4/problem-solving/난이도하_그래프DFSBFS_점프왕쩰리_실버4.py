4af2673f196e9aca0adddb69cddad7b7043f9f19
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-22 18:35:04 +0900

    solve: boj-16173-점프왕쩰리

--- SW-AI-W02-05/week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ SW-AI-W02-05/week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,25 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+def dfs(x, y):
+    if x >= n or y >= n or visited[x][y]:
+        return False
+    
+    if arr[x][y] == -1:
+        return True
+    
+    visited[x][y] = True
+    jump = arr[x][y]
+
+    return dfs(x, y + jump) or dfs(x + jump, y)
+
+if dfs(0, 0):
+    print("HaruHaru")
+else:
+    print("Hing")
