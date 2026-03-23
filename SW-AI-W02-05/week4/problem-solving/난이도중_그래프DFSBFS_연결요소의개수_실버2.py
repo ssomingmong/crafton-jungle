b18b919d8ad1aa152fbcc8b109bd5111e0e0031c
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-23 16:49:44 +0900

    solve: boj-11724-연결 요소의 개수

--- SW-AI-W02-05/week4/problem-solving/난이도중_그래프DFSBFS_연결요소의개수_실버2.py
+++ SW-AI-W02-05/week4/problem-solving/난이도중_그래프DFSBFS_연결요소의개수_실버2.py
@@ -1,2 +1,31 @@
 # 그래프, DFS, BFS - 연결 요소의 개수 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/11724
+
+import sys
+sys.setrecursionlimit(1001)
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = {i:[] for i in range(1, n + 1)}
+visited = {i:False for i in graph}
+count = 0
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+def dfs(node):
+    visited[node] = True
+
+    for nxt in graph[node]:
+        if not visited[nxt]:
+            dfs(nxt)
+
+for j in range(1, n + 1):
+    if not visited[j]:
+        count += 1
+        dfs(j)
+
+print(count)
