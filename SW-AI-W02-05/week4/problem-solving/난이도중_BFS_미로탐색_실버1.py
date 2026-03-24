cf68954f69f4352bfd8b1b74694283e153578cc0
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-24 16:20:07 +0900

    solve: boj-2178-미로 탐색

--- SW-AI-W02-05/week4/problem-solving/난이도중_BFS_미로탐색_실버1.py
+++ SW-AI-W02-05/week4/problem-solving/난이도중_BFS_미로탐색_실버1.py
@@ -1,2 +1,32 @@
 # BFS - 미로 탐색 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/2178
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+maze = [list(map(int, input().strip())) for _ in range(m)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(start_x, start_y):
+    queue = deque([(start_x, start_y)])
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if maze[nx][ny] == 1:
+                    maze[nx][ny] = maze[x][y] + 1
+                    queue.append((nx, ny))
+
+    return maze[n-1][m -1]
+
+print(bfs(0, 0))
