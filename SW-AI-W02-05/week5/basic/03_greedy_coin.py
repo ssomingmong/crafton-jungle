793508baaa380ae91a593f3a28dc81badcf6be8a
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-27 17:25:28 +0900

    solve: basic-03-greedy-coins

--- SW-AI-W02-05/week5/basic/03_greedy_coin.py
+++ SW-AI-W02-05/week5/basic/03_greedy_coin.py
@@ -46,7 +46,13 @@ def make_change_greedy(change, coins):
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
+    for i in range(len(coins)):
+        count = change // coins[i]
+        change = change % coins[i]
+        
+        if count > 0:
+            result[coins[i]] = count
+            total_coins += count
     
     return total_coins, result
 
