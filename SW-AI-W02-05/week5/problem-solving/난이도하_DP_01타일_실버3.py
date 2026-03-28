29b6f49351ee788fe69195ae9d66f3432dd1544c
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-28 13:22:19 +0900

    solve: boj-1904-01타일

--- SW-AI-W02-05/week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ SW-AI-W02-05/week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,20 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+def tile(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    
+    a, b = 1, 2
+    for i in range(3, n + 1):
+        a, b = b, (a + b) % 15746
+    
+    return b
+
+print(tile(n))
