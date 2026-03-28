3ac67ee8528130b8918015e7799828c226e367fe
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-28 13:34:37 +0900

    solve: boj-11047-동전0

--- SW-AI-W02-05/week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ SW-AI-W02-05/week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,20 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+import sys
+input = sys.stdin.readline
+
+n , k = map(int, input().split())
+coins = []
+count = 0
+
+for _ in range(n):
+    coins.append(int(input()))
+
+coins.sort(reverse=True)
+
+for i in coins:
+    count += k // i
+    k = k % i
+
+print(count)
