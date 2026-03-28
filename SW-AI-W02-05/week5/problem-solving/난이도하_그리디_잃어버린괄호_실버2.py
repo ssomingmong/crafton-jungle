aef35fcd8677b0a91f3500425605a9903c41da78
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-28 14:03:45 +0900

    solve: boj-1541-잃어버린 괄호

--- SW-AI-W02-05/week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ SW-AI-W02-05/week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,20 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+import sys
+input = sys.stdin.readline
+
+expression = input()
+result = 0
+
+a = expression.split('-')   
+
+for i in range(len(a)):
+    group_sum = sum(map(int, a[i].split('+')))
+
+    if i == 0:
+        result += group_sum
+    else:
+        result -= group_sum
+
+print(result)
