b7547cb3421cf04ea4f08b203284a5d943c94e56
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-28 11:03:28 +0900

    solve: boj-2748-피보나치 수2

--- SW-AI-W02-05/week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ SW-AI-W02-05/week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,19 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+fibo_dp = {0:0, 1:1}
+
+def fibo(n):
+    if n in fibo_dp:
+        return fibo_dp[n]
+    
+    nth_dp = fibo(n - 1) + fibo(n - 2)
+    fibo_dp[n] = nth_dp
+
+    return nth_dp
+
+print(fibo(n))
