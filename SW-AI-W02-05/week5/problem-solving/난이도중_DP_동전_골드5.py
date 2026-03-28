bcef09787afab2e1124fc32e9760c9b2df519cee
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-28 21:44:08 +0900

    solve: boj-9084-동전

--- SW-AI-W02-05/week5/problem-solving/난이도중_DP_동전_골드5.py
+++ SW-AI-W02-05/week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,24 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    m = int(input())
+
+    dp = [0] * (m + 1)
+    dp[0] = 1
+
+    for coin in coins:
+        for i in range(coin, m + 1):
+
+            # 현재 층수에서 내 블록만큼 뺀 아래층의 정답이다.
+            dp[i] += dp[i - coin]
+
+    print(dp[m])
+    
