b014492b97053788053124603ef32e518f7867f9
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-31 21:08:30 +0900

    solve: boj-9251-LCS

--- SW-AI-W02-05/week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ SW-AI-W02-05/week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,19 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+import sys
+input = sys.stdin.readline
+
+s1 = input()
+s2 = input()
+
+dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+for i in range(1, len(s1) + 1):
+    for j in range(1, len(s2) + 1):
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[len(s1)][len(s2)])  
